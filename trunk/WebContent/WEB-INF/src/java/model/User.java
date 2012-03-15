2434c16b279961aa011553d2cb08941ebb9d6a59
vidyasekar.s@gmail.com <vidyasekar.s@gmail.com@1ff4df13-5bda-0a29-1c40-c22020fe81cd>
2012-03-15 15:03:40 +0000

    Changed. Mistakes in session uid, fixed

--- trunk/WebContent/WEB-INF/src/java/model/User.java
+++ trunk/WebContent/WEB-INF/src/java/model/User.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.JOptionPane;
+
 //import com.login.vo.LoginVo;
 import util.DB;
 import util.MyLog;
@@ -13,33 +15,31 @@ import com.opensymphony.xwork2.ActionContext;
 
 public class User {
 	
-	
 	public List verifyDetails(String username,String password){
 	List lstRegister = new ArrayList();
-	System.out.println("password is " +password);
+	//System.out.println("password is " +password);
 	ResultSet resultSet = null;
-	String query = "select user_name,user_id, description from user where user_name='" + username+"'"; 
-			//+ "' and Password='" + password + "'";
-	System.out.println(""+query);
+	String query = "select user_name,user_id, description from user where user_name='" + username+"'" + " and password='" + password + "'";
+	//System.out.println(""+query);
 	
 	Connection connection = DB.getConnection();
 	resultSet = DB.readFromDB(query, connection);
 	
 	try {
-		System.out.println(""+resultSet.getFetchSize());
+		//System.out.println(""+resultSet.getFetchSize());
 		while (resultSet.next()) {
-			System.out.println("i am here");
+			//System.out.println("i am here");
 			String userName = resultSet.getString("user_name");
 			String userid = resultSet.getString("user_id");
 			String f_name = resultSet.getString("description");
-			System.out.println(""+userName);
+			//System.out.println(""+userName);
 			//LoginVo loginVo = new LoginVo(userName,userid, f_name);
-			System.out.println(""+f_name);
-			
+			//System.out.println(""+f_name);
+			//JOptionPane.showMessageDialog(null, userid);
 			Map session = ActionContext.getContext().getSession();
-			session.put("first_name",f_name);
+			//session.put("first_name",f_name);
 			session.put("uid",userid);
-			System.out.println("user id"+userid);
+			//System.out.println("user id"+userid);
 			lstRegister.add(userName);
 			return lstRegister;
 			//lstRegister.add(loginVo);
